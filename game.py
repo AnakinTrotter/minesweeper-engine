f5972b54a9898863e2467579b46cd16d03b132e1
Henry Kam <henry.j.kam@gmail.com>
2022-01-03 17:42:48 -0800

    generate grid

--- game.py
+++ game.py
@@ -1,11 +1,39 @@
 import numpy as np
+import random
+
 # TODO: add time
-# n is width
+
 grid = []
-def generateGrid(row, col, bombs):
+def generateBombs(bomb):
+    bombs = set()
+    for i in range(bomb):
+        bombRow = random.randint(0,row-1)
+        bombCol = random.randint(0, col-1)
+        while(bombRow, bombCol) in bombs:
+            bombRow = random.randint(0,row-1)
+            bombCol = random.randint(0, col-1)
+        bombs.add((bombRow, bombCol))
+    return list(bombs)
+
+def generateGrid(row, col, bomb):
     global grid
+    # create grid with zeroes
     grid = np.zeros((row, col))
-    print(grid)
+    bombs = np.asarray(generateBombs(bomb))
+
+    for mine in bombs:
+        i= mine[0]
+        j= mine[1]
+
+        pts = [[i-1, j], [i+1, j], [i , j+1], [i , j-1], [i-1, j+1], [i+1, j+1], [i+1, j-1], [i-1, j-1]]
+
+        for cols in pts:
+            if cols[0]>=0 and cols[0] < row and cols[1]>=0 and cols[1] < col:
+                grid[cols[0]][cols[1]]+=1
+
+    for i in bombs:
+        grid[i[0]][i[1]] = -1
+        
 
 def prompt(promptType):
     while True:
@@ -28,8 +56,9 @@ def prompt(promptType):
         else:
             break
     return value
-    
+
 row = prompt("rows")
 col = prompt("columns")
 bomb = prompt("bombs")
 generateGrid(row, col, bomb)
+print(grid)
